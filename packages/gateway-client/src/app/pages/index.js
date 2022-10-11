36eec1151493d240a302d400fc0f47eb6659bbba
Joshua Carter <carter.joshua.603@gmail.com>
2022-10-11 09:18:53 -0700

    Improve default page react logic

--- packages/gateway-client/src/app/pages/index.js
+++ packages/gateway-client/src/app/pages/index.js
@@ -5,46 +5,9 @@ import Trust from '../components/trust';
 import BasicLayout from '../layouts/basic';
 import { getSupportedPlatform, isPwa, isSupportedPlatform } from './support';
 
-const WELCOME_STATUS = `http://pleroma.3b836fd1ff44e0fca4768822415ef6614d0c0d4f07b96008b1367734161e628.f.yg/api/v1/statuses/AMZ2VxYnjpo5sK8W00`;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const WELCOME_STATUS_PAGE = `/@ryan@pleroma.3b836fd1ff44e0fca4768822415ef6614d0c0d4f07b96008b1367734161e628.f.yg/posts/AMZ2VxYnjpo5sK8W00`;
-
-// let timeout = null;
-let _reloading = false;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function waitForWelcomeAvailable() {
-    while (true) {
-        const res = await fetch(WELCOME_STATUS).catch(e => {
-            console.warn(e);
-            return null;
-        });
-        if (res && res.ok) {
-            return;
-        }
-        await new Promise(r => setTimeout(r, 2000));
-    }
-}
-
-async function getLoadUrl() {
-    // const welcome = await localStorage.getItem('welcome')
-
-    // await waitForWelcomeAvailable()
-
-    return '/';
-}
-
 async function reload() {
-    // alert('reload fn')
-    if (isPwa() && document.visibilityState === 'visible') {
-        // alert('wait for active worker')
-        while (navigator.serviceWorker?.controller?.state !== 'activated')
-            await new Promise(r => setTimeout(r, 100));
-        // alert('reload')
-        if (!_reloading) {
-            _reloading = true;
-            window.location.href = await getLoadUrl();
-        }
+    if (document.visibilityState === 'visible') {
+        await sendReload();
     }
 }
 
@@ -53,26 +16,62 @@ async function registerReloader() {
     // alert('add visibility listener')
     document.addEventListener('visibilitychange', reload);
     // alert('try reload')
-    return reload();
 }
 
 registerReloader().catch(e => {
     alert(e.message);
 });
 
+async function sendReload(
+    setSanity = m => {
+        console.log(m);
+    }
+) {
+    setSanity('wait for controller');
+    let i = 0;
+    let swstate = navigator.serviceWorker?.controller?.state;
+    while (swstate !== 'activated') {
+        setSanity(
+            `waiting for controller ${++i} ${
+                navigator.serviceWorker?.controller?.state
+            }`
+        );
+        swstate = navigator.serviceWorker?.controller?.state;
+        await new Promise(r => setTimeout(r, 1000));
+    }
+    const url = new URL(window.location.href);
+    const address = url.searchParams.get('mdns');
+    setSanity('send reload message to worker');
+    navigator.serviceWorker.onmessage = msg => {
+        if (msg.data === 'NAVIGATE') {
+            window.location.href = '/';
+        }
+    };
+
+    if (address) {
+        console.log('postmessage');
+        navigator.serviceWorker.controller.postMessage({
+            type: 'MDNS',
+            address,
+        });
+    } else {
+        navigator.serviceWorker.controller.postMessage({
+            type: 'START',
+        });
+    }
+}
+
 export default function Home() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [platform, setPlatform] = useState('');
     const [recommended, setRecommended] = useState('');
-    const [reloading, setReloading] = useState(false);
+    const [reloading] = useState(false);
     const [error] = useState(null);
     const [sanity, setSanity] = useState('insane2');
 
     useEffect(() => {
         setSanity('insane in the membrane');
-        if (isPwa()) {
-            setReloading(true);
-            reload();
-        } else if (isSupportedPlatform()) {
+        if (isSupportedPlatform()) {
             setPlatform(getSupportedPlatform());
         } else {
             setRecommended(getSupportedPlatform());
@@ -80,36 +79,20 @@ export default function Home() {
     }, [reloading]);
 
     useEffect(() => {
-        (async function () {
-            console.log('wait for controller');
-            while (navigator.serviceWorker?.controller?.state !== 'activated') {
-                console.log('wait', navigator.serviceWorker?.controller);
-                await new Promise(r => setTimeout(r, 1000));
-            }
-            const url = new URL(window.location.href);
-            console.log('url');
-            const address = url.searchParams.get('mdns');
-            if (address) {
-                console.log('postmessage');
-                navigator.serviceWorker.controller.postMessage({
-                    type: 'MDNS',
-                    address,
-                });
-            } else {
-                navigator.serviceWorker.controller.postMessage({
-                    type: 'START',
-                });
-            }
-        })();
+        if (isPwa()) {
+            sendReload(setSanity);
+        } else {
+            setSanity('please install PWA to continue');
+        }
     }, []);
 
     if (recommended) {
         return <Unsupported recommended={recommended} />;
     }
 
-    if (platform) {
-        return <Instructions platform={platform} />;
-    }
+    // if (platform) {
+    //     return <Instructions platform={platform} />;
+    // }
 
     return (
         <BasicLayout>
@@ -129,6 +112,7 @@ function Unsupported({ recommended }) {
     );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Instructions({ platform }) {
     if (platform === 'Chrome') {
         return (

48e68a1c4c79b2608887db5867b3a34b83d412ed
Joshua Carter <carter.joshua.603@gmail.com>
2022-10-17 15:17:46 -0700

    Report status of service worker connection logic to redux slice

--- packages/gateway-client/src/app/redux/service-worker/serviceWorker.logic.ts
+++ packages/gateway-client/src/app/redux/service-worker/serviceWorker.logic.ts
@@ -1,18 +1,78 @@
 import { register } from '@athena/shared/service-worker';
+import {
+    Message,
+    MessageType,
+    ServerPeerStatus,
+} from '../../../service-worker';
 import { AppDispatch } from '../store';
-import { setIsControlling, setStatus } from './serviceWorker.slice';
+import {
+    setIsControlling,
+    setRelayAddresses,
+    setServerPeerStatus,
+    setStatus,
+} from './serviceWorker.slice';
 
 export class ServiceWorkerLogic {
-    private updateStatus(dispatch: AppDispatch, worker: ServiceWorker) {
-        dispatch(setStatus(worker.state));
+    private handlers: Record<string, EventListener> = {};
+
+    private worker: ServiceWorker | null = null;
+
+    private messageHandlers: Record<
+        MessageType,
+        (msg: Message, dispatch: AppDispatch) => void
+    > = {
+        SERVER_PEER_STATUS: (msg: Message, dispatch: AppDispatch) => {
+            dispatch(setServerPeerStatus(msg.status as ServerPeerStatus));
+        },
+        LOADED_RELAYS: (msg: Message, dispatch: AppDispatch) => {
+            dispatch(setRelayAddresses(msg.relays as string[]));
+        },
+    };
+
+    private updateStatus(dispatch: AppDispatch) {
+        if (!this.worker) {
+            throw new Error('Missing worker in updateStatus()');
+        }
+        dispatch(setStatus(this.worker.state));
         // check if controlling
         dispatch(
-            setIsControlling(navigator.serviceWorker.controller === worker)
+            setIsControlling(navigator.serviceWorker.controller === this.worker)
+        );
+    }
+
+    private handleMessage(e: MessageEvent<Message>, dispatch: AppDispatch) {
+        const msg = e.data;
+        if (!MessageType[msg.type]) {
+            console.warn(
+                'Ignoring service worker message with unknown type: ' + msg.type
+            );
+        }
+        this.messageHandlers[msg.type](msg, dispatch);
+    }
+
+    private updateEventListener(event: string, handler: EventListener) {
+        if (this.handlers[event]) {
+            navigator.serviceWorker.removeEventListener(event, handler);
+        }
+        this.handlers[event] = handler;
+        navigator.serviceWorker.addEventListener(event, handler);
+    }
+
+    private registerContainerListeners(dispatch: AppDispatch) {
+        // remove old event listeners and add new ones
+        this.updateEventListener('message', e =>
+            this.handleMessage(e as MessageEvent, dispatch)
         );
     }
 
     async registerServiceWorker(dispatch: AppDispatch) {
-        // first register our service worker
+        // remove previous worker state
+        this.worker = null;
+
+        // register listeners on our container first
+        this.registerContainerListeners(dispatch);
+
+        // next, register our service worker
         const registration = await new Promise<ServiceWorkerRegistration>(
             resolve => {
                 register({
@@ -22,12 +82,12 @@ export class ServiceWorkerLogic {
         );
 
         // now get our worker
-        const worker =
+        this.worker =
             registration.installing ??
             registration.waiting ??
             registration.active;
         // if we couldn't
-        if (!worker) {
+        if (!this.worker) {
             throw new Error(
                 `Unable to retrieve service worker from registration.`
             );
@@ -35,11 +95,11 @@ export class ServiceWorkerLogic {
 
         // by now, we have our worker
         //update state
-        this.updateStatus(dispatch, worker);
+        this.updateStatus(dispatch);
 
         // attach event listeners
-        worker.addEventListener('statechange', () => {
-            this.updateStatus(dispatch, worker);
+        this.worker.addEventListener('statechange', () => {
+            this.updateStatus(dispatch);
         });
     }
 }
--- packages/gateway-client/src/app/redux/service-worker/serviceWorker.slice.ts
+++ packages/gateway-client/src/app/redux/service-worker/serviceWorker.slice.ts
@@ -1,14 +1,47 @@
 import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ServerPeerStatus } from '../../../service-worker';
 
 import { RootState } from '../store';
+
+/**
+ * Service worker lifecycle:
+ *
+ * 1. Executed
+ * 2. Get server peer:
+ *   a. Fetch bootstrap address
+ *   b. Fetch relay list
+ *   c. Start libp2p node (w/ autodial effectively disabled)
+ *   d. Connect (probably) to peers on peer:discovery event
+ *   e. Receive peer:connect event filtered to be our server (self.serverPeer)
+ * 3. Get list of relays:
+ *   a. Open stream to `/samizdapp-relay`
+ *   b. For each received address:
+ *     i. Add to stored relay list
+ *     ii. Add to libp2p node address book
+ *
+ */
+
+/**
+ * Service worker actions:
+ *
+ * - Make a fetch request:
+ *   - Waits on lifecycle: (2. Get server peer)
+ *   - Sends request through p2p stream
+ *
+ * - Receive a message:
+ *   - Set started true
+ */
 
 export interface WorkerState {
     status?: ServiceWorkerState;
     isControlling: boolean;
+    serverPeerStatus?: ServerPeerStatus;
+    relayAddresses: string[];
 }
 
 const initialState: WorkerState = {
     isControlling: false,
+    relayAddresses: [],
 };
 
 export const serviceWorkerSlice = createSlice({
@@ -28,18 +61,43 @@ export const serviceWorkerSlice = createSlice({
         ) => {
             state.isControlling = isControlling;
         },
+        setServerPeerStatus: (
+            state,
+            { payload: status }: PayloadAction<ServerPeerStatus>
+        ) => {
+            state.serverPeerStatus = status;
+        },
+        setRelayAddresses: (
+            state,
+            { payload: addresses }: PayloadAction<string[]>
+        ) => {
+            state.relayAddresses = addresses;
+        },
     },
 });
 
-export const { setIsControlling, setStatus } = serviceWorkerSlice.actions;
+export const {
+    setIsControlling,
+    setStatus,
+    setServerPeerStatus,
+    setRelayAddresses,
+} = serviceWorkerSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.serviceWorker.value)`
 export const selectServiceWorker = (state: RootState) => state.serviceWorker;
-export const selectStatus = createSelector(
+export const selectWorkerStatus = createSelector(
     selectServiceWorker,
-    state => state.status
+    state => ({
+        status: state.status,
+        isControlling: state.isControlling,
+        serverPeerStatus: state.serverPeerStatus,
+    })
+);
+export const selectRelayAddresses = createSelector(
+    selectServiceWorker,
+    state => state.relayAddresses
 );
 
 export default serviceWorkerSlice.reducer;

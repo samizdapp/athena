206f0adc1dc4901b4cca62da5fd16acb8b60ebc3
Ryan Bennett <your@mail.com>
2022-10-18 14:50:25 -0600

    format

--- packages/shared/service-worker/src/lib/registration.ts
+++ packages/shared/service-worker/src/lib/registration.ts
@@ -17,12 +17,12 @@
 
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+        // [::1] is the IPv6 localhost address.
+        window.location.hostname === '[::1]' ||
+        // 127.0.0.0/8 are considered localhost for IPv4.
+        window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        )
 );
 
 type Config = {
@@ -51,19 +51,18 @@ export function register(config?: Config) {
             navigator.serviceWorker.ready.then(() => {
                 console.log(
                     'This web app is being served cache-first by a service ' +
-                    'worker.'
+                        'worker.'
                 );
             });
         } else {
             // Is not localhost. Just register service worker
-            registerValidSW(swUrl, config).catch((e) => {
-                console.log('error', e)
-                registerValidSW('../service-worker.js', config)
+            registerValidSW(swUrl, config).catch(e => {
+                console.log('error', e);
+                registerValidSW('../service-worker.js', config);
             });
         }
     });
 }
-
 
 async function registerValidSW(swUrl: string, config?: Config) {
     return navigator.serviceWorker
@@ -84,7 +83,7 @@ async function registerValidSW(swUrl: string, config?: Config) {
                             // content until all client tabs are closed.
                             console.log(
                                 'New content is available and will be used when all ' +
-                                'tabs for this page are closed.'
+                                    'tabs for this page are closed.'
                             );
 
                             // Execute callback
@@ -105,7 +104,7 @@ async function registerValidSW(swUrl: string, config?: Config) {
                     }
                 };
             };
-        })
+        });
 }
 
 function checkValidServiceWorker(swUrl: string, config?: Config) {

c25c198f7e532fb0b72b982bff2c1fb3ef6aa185
Joshua Carter <carter.joshua.603@gmail.com>
2022-10-26 11:46:16 -0700

    Correct errors in bootstrap logic (fix libp2p connection issues) (#21)
    * Copy libp2p.* files to assets directory
    
    * Rewrite logic for fetching bootstrap address to always check for new address

--- packages/gateway-client/copy.sh
+++ packages/gateway-client/copy.sh
@@ -4,5 +4,5 @@ cd /usr/src
 mv ./volumes/gateway_client/assets/libp2p.* .
 rm -rf ./volumes/gateway_client/*
 cp -rf ./athena/packages/gateway-client/. ./volumes/gateway_client
-mv libp2p.* /usr/src/volumes/gateway_client
+mv libp2p.* ./volumes/gateway_client/assets
 sleep infinity

114c852f8561172bf5c5f0f9448a1372ab5a07f6
Joshua Carter <carter.joshua.603@gmail.com>
2022-11-03 10:24:42 -0700

    Revert "SAM-41-new-athena-base-image (#29)"
    This reverts commit 073f11579b15ebd9bfa08548009100f3dcfdbe19.

--- packages/gateway-client/copy.sh
+++ packages/gateway-client/copy.sh
@@ -3,6 +3,6 @@
 cd /usr/src
 mv ./volumes/gateway_client/assets/libp2p.* .
 rm -rf ./volumes/gateway_client/*
-cp -rf ./athena/dist/packages/gateway-client/. ./volumes/gateway_client
+cp -rf ./athena/packages/gateway-client/. ./volumes/gateway_client
 mv libp2p.* ./volumes/gateway_client/assets
 sleep infinity

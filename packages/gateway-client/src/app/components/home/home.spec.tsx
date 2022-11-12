afcf479b5440bd31714446fac8a5a5b262055fd2
Joshua Carter <carter.joshua.603@gmail.com>
2022-11-11 16:13:12 -0800

    SAM-63 automated testing (#43)
    * Get current boilerplate tests to pass (setup redux testing)
    
    * Add new test job to validation workflow

--- packages/gateway-client/src/app/components/home/home.spec.tsx
+++ packages/gateway-client/src/app/components/home/home.spec.tsx
@@ -1,10 +1,17 @@
-import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import 'whatwg-fetch';
 
+import { renderWithProviders } from '../../redux/testing';
 import Home from './home';
 
+jest.mock('../../support');
+
 describe('Home', () => {
+    const renderWithRouter = (children: React.ReactNode) =>
+        renderWithProviders(<MemoryRouter>{children}</MemoryRouter>);
+
     it('should render successfully', () => {
-        const { baseElement } = render(<Home />);
+        const { baseElement } = renderWithRouter(<Home />);
         expect(baseElement).toBeTruthy();
     });
 });
--- packages/gateway-client/src/app/components/status/status.spec.tsx
+++ packages/gateway-client/src/app/components/status/status.spec.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import 'whatwg-fetch';
 
+import { renderWithProviders } from '../../redux/testing';
 import Status from './status';
 
 describe('Status', () => {
     it('should render successfully', () => {
-        const { baseElement } = render(<Status />);
+        const { baseElement } = renderWithProviders(<Status />);
         expect(baseElement).toBeTruthy();
     });
 });

3344a4f8afab157bddef40283bcf2193fd2f5818
Joshua Carter <carter.joshua.603@gmail.com>
2022-12-02 13:27:05 -0800

    SAM-78 supervisor script (#66)
    * Configure Node and Python versions with nvm and conda and update README.md instructions
    
    * Configure .yml formatting in .prettierrc
    
    * Configure Python and Poetry in athena
    
    * Create supervisory.py script that syncs docker compose state with manifests API
    
    * Create new supervisor nest module to handle running the supervisory script periodically
    
    * Return on 409 on duplicate manifest name
    
    * Ignore manifests.json file

--- packages/app-service/src/app/app.module.ts
+++ packages/app-service/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+
 import { ManifestsModule } from '../manifests/manifests.module';
+import { SupervisorModule } from '../supervisor/supervisor.module';
 
 @Global()
 @Module({
-    imports: [ManifestsModule],
+    imports: [ManifestsModule, SupervisorModule],
     providers: [
         {
             provide: APP_PIPE,

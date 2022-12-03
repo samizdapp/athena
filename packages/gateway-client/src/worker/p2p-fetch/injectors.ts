f1f6d19305975737f44c9e5c1d5777f708427929
Ryan Bennett <nomad.ry@gmail.com>
2022-12-02 20:10:44 -0700

    Rb injectors (#65)
    * use v2 streams
    
    * stream request fixes
    
    * initial injector pattern
    
    * remove dead code
    
    * remove dead code
    
    * ios monitor logging
    
    * revert injectors
    
    * bring back injectors
    
    * remove lint error
    
    * copy deferred
    
    * Update packages/gateway-client/src/worker/p2p-fetch/p2p-fetch-request.ts
    
    Co-authored-by: Joshua Carter <carter.joshua.603@gmail.com>
    
    * bring back timing with request streams
    
    * moar better
    
    * MOAR BETTER
    
    * fix ios injector syntax
    
    * fix formatting
    
    * update content length in SW_MONITOR injector
    
    * comments
    
    * less whitespace
    
    * rename monitor -> heartbeat
    
    * add useragent check
    
    * rename heartbeat enums
    
    Co-authored-by: Ryan Bennett <your@mail.com>
    Co-authored-by: Joshua Carter <carter.joshua.603@gmail.com>

---
+++ packages/gateway-client/src/worker/p2p-fetch/injectors.ts
@@ -0,0 +1,78 @@
+import { ClientMessageType, WorkerMessageType } from '../../worker-messaging';
+import messenger from '../messenger';
+
+declare type Injector = (headers: Headers, body: Buffer) => Buffer;
+
+class Injectors {
+    constructor(private readonly injectors: Injector[]) {}
+
+    inject(headers: Headers, body: Buffer): Buffer {
+        for (const injector of this.injectors) {
+            body = injector(headers, body);
+        }
+        return body;
+    }
+}
+
+// HEARTBEAT Injector
+
+// iOS service workers sporatically become unresponsive when the app is in the background
+// this injector injects a script into any html page that will check for responsiveness
+// and prompt the user to restart the app if the service worker is unresponsive
+
+// responsiveness is tested by echoing a message back to the parent page
+messenger.addListener(ClientMessageType.HEARTBEAT, () => {
+    messenger.broadcastMessage({
+        type: WorkerMessageType.HEARTBEAT,
+    });
+});
+
+// we want to inject this script into the parent page at the very top of the <head> tag
+const HEARTBEAT_SPLIT = '<head>';
+
+// this is the script that will be injected, it issues a postMessage to the service worker
+// when the document becomes visible, and listens for a response within 1 second
+const HEARTBEAT_SNIPPET = `<script>
+    if (navigator.userAgent.includes('iPhone')) {
+        document.addEventListener("visibilitychange", () => {
+            console.log('visibilitychange', document.visibilityState, navigator.serviceWorker.controller?.state);
+            if (document.visibilityState === 'visible' && navigator.serviceWorker.controller?.state === 'activated') {
+                console.log('test service worker responsiveness');
+                navigator.serviceWorker.controller?.postMessage({
+                    type: '${ClientMessageType.HEARTBEAT}'
+                });
+                const start = Date.now();
+                const timeout = setTimeout(() => {
+                    alert('Service worker is unresponsive, please restart the app');
+                }, 1000);
+                navigator.serviceWorker.onmessage = (e) => {
+                    if (e.data.type === '${WorkerMessageType.HEARTBEAT}') {
+                        console.log('Service worker is responsive', Date.now() - start);
+                        clearTimeout(timeout);
+                    }
+                };
+            }
+        });
+    }
+</script>`;
+
+// this is the injector function that will be called for each response
+const injectSWHeartbeat: Injector = (headers, body) => {
+    // check if the response is html
+    if (headers.get('content-type')?.startsWith('text/html')) {
+        const [start, end] = body.toString().split(HEARTBEAT_SPLIT);
+        // check if the response contains the <head> tag
+        if (start && end) {
+            const parts = [start, HEARTBEAT_SPLIT, HEARTBEAT_SNIPPET, end];
+            const newBody = Buffer.from(parts.join(''));
+            // update the headers to have the correct content-length
+            headers.set('content-length', newBody.byteLength.toString());
+            return newBody;
+        }
+    }
+
+    // if the response is not html or does not contain the <head> tag, return the original body
+    return body;
+};
+
+export default new Injectors([injectSWHeartbeat]);

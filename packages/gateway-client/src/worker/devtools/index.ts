fa4ffcb6e2205a3066f32b1200a1eddeb6839124
Joshua Carter <carter.joshua.603@gmail.com>
2022-12-07 19:46:16 -0800

    Make worker version available in SamizdAppDevTools

--- packages/gateway-client/src/worker/devtools/index.ts
+++ packages/gateway-client/src/worker/devtools/index.ts
@@ -1,17 +1,20 @@
 import localforage from 'localforage';
 
+import { WorkerVersionManifest } from '../../worker-messaging';
 import * as logging from '../logging';
 import { P2pClient } from '../p2p-client';
 import status from '../status';
 
 declare const self: {
     SamizdAppDevTools: SamizdAppDevTools;
+    version: WorkerVersionManifest;
 } & ServiceWorkerGlobalScope;
 
 export class SamizdAppDevTools {
     public logging = logging;
     public status = status;
     public localforage = localforage;
+    public version = self.version;
 
     constructor(public p2pClient: P2pClient) {
         // attach to window

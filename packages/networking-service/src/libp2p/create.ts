e7a80b4eb52052abe17db73eb5d41a3de915aa30
Ryan Bennett <your@mail.com>
2022-12-10 22:22:14 -0700

    wip

--- packages/networking-service/src/libp2p/create.ts
+++ packages/networking-service/src/libp2p/create.ts
@@ -1,14 +1,13 @@
 
-import { createLibp2p, Libp2p } from "libp2p";
-import {
-  createEd25519PeerId,
-  createFromProtobuf,
-  exportToProtobuf,
-} from "@libp2p/peer-id-factory";
+// eslint-disable-next-line
+// @ts-ignore
+import { libp2p,  createEd25519PeerId, createFromProtobuf,exportToProtobuf } from "libp2p";
 import { readFile, writeFile} from "fs/promises";
 import { environment } from "../environments/environment";
 
-export default async function createLibp2pInstance(): Promise<Libp2p> {
+export const createLibp2p = libp2p.createLibp2p;
+
+export default async function createLibp2pInstance(): Promise<libp2p.Libp2p> {
     const peerId = await readFile(environment.libp2p_id_file)
             .then(createFromProtobuf)
             .catch(async (_) => {

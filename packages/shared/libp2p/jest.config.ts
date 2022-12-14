61a1ed5ed0446c4de400947f7c5e066906a7c57a
Ryan Bennett <your@mail.com>
2022-12-13 20:30:00 -0700

    shared libp2p jest fix

--- packages/shared/libp2p/jest.config.ts
+++ packages/shared/libp2p/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
     displayName: 'libp2p',
-    preset: '../../jest.preset.js',
+    preset: '../../../jest.preset.js',
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,5 +12,5 @@ export default {
         '^.+\\.[tj]sx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/packages/libp2p',
+    coverageDirectory: '../../../coverage/packages/libp2p',
 };
